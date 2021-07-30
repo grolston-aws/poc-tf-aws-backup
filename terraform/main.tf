663f60a3258827cdc17a6c0f53300199e259c870
George Rolston <grolston@outlook.com>
2021-07-30 10:41:08 -0600

    formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 locals {
-    account_id = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_iam_role" "aws_backup" {
